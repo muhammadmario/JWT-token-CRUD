0c2927137602134ec39d5ad6e4b61e1e18f49b80
Muhammad Mario <acernitro58@gmail.com>
2022-07-12 22:45:11 +0700

    menambah role user

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -23,7 +23,7 @@ const handleNewUser = async (req, res) => {
     // encrypt pwd
     const hashedPwd = await bcrypt.hash(pwd, 10);
     //store the new user
-    const newUser = { username: user, password: hashedPwd };
+    const newUser = { username: user, roles: { User: 1 }, password: hashedPwd };
     usersDB.setUsers([...usersDB.users, newUser]);
     await fsPromises.writeFile(
       path.join(__dirname, "..", "model", "users.json"),
